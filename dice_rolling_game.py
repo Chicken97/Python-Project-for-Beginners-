652783ed2a132adbdca7e6b1ca51f36513370434
Al Amin Sami <145697446+Chicken97@users.noreply.github.com>
2024-09-13 03:13:46 +0600

    Update dice_rolling_game.py

--- dice_rolling_game.py
+++ dice_rolling_game.py
@@ -5,14 +5,16 @@ def dice_rolling_game(choice):
         print(f"Player One : {randint(1, 6)} and Player Two : {randint(1, 6)}")
     elif choice == 'n':
         print("Thank you for playing!")
-        exit()
+        return
     else:
         print("Invalid choice! Please enter 'y' or 'n'")
 
 def main():
     while True:
-        user_input = input("Roll the dice? (y/n:) ").lower()
+        user_input = input("Roll the dice? (y/n): ").lower()
         dice_rolling_game(user_input)
+        if user_input == 'n':
+            break
 
 if __name__ == '__main__':
     main()
